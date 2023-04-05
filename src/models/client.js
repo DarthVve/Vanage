67116040ffce1e7ec46af29f808b8fb4ed527b71
Darth Vve <osheviremajoroh@gmail.com>
2023-04-06 00:14:24 +0100

    0.0.3

--- src/models/client.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const clientSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String },
-    phone: { type: String }
-});
-
-module.exports = mongoose.model('Client', clientSchema);
